bc4d4c5451d2246ac31f4191f46db1ace7f3865f
Dela3a <46228597+Dela3a@users.noreply.github.com>
2023-12-03 18:24:43 +0100

    update

--- game_of_life.py
+++ game_of_life.py
@@ -3,7 +3,6 @@ import sys
 import numpy as np
 from copy import deepcopy
 from random import randint
-from tkinter import Tk, filedialog  # Import tkinter modules
 
 def createArray(widthSquares, heightSquares):
     return [[random.randint(0, 1) for _ in range(widthSquares)] for _ in range(heightSquares)]
@@ -15,30 +14,19 @@ def countNeigbors(array, counts, x, y):
             count+=array[l][o]
     count -= array[y][x]
     counts[y][x] = count
-    #print(array[y][x], count)
-    #count = sum(array[y-1:y+1][x-1:x+2])
     return counts
 
 def gameOfLifeRules(array, counts, x, y, ):
     gamed = deepcopy(array)
     count = counts[y][x]
-    #print(f'Before {array[y][x]}, count {count}')
     if array[y][x] == 0 and count == 3:
         gamed[y][x] = 1
-        #print("reproduction")
-        #print(array[y][x], count, gamed[y][x])
     elif array[y][x] == 1 and (count == 2 or count == 3):
         gamed[y][x] = 1
-        #print("next gen")
-        #print(array[y][x], count, gamed[y][x])
     elif array[y][x] == 1 and count > 3:
         gamed[y][x] = 0
-        #print('over-pop')
-        #print(array[y][x], count, gamed[y][x])
     elif array[y][x] == 1 and count < 2:
         gamed[y][x] = 0
-        #print('under-pop')
-        #print(array[y][x], count, gamed[y][x])
     return gamed
 
 def saveNpyArray(array):
@@ -48,15 +36,13 @@ def loadNpyArray(array):
     filename = 'test.npy'
     try:
         loaded_array = np.load(filename)
-        if loaded_array.shape == array.shape:  # Ensure shapes match before updating
-            array[:] = loaded_array  # Update the array
+        if loaded_array.shape == array.shape:
+            array[:] = loaded_array
     except Exception as e:
         print("Error loading file:", e)
 
 
-# Initialize Pygame
 pygame.init()
-# Define Button class
 class Button:
     def __init__(self, x, y, width, height, color, text, function=None):
         self.rect = pygame.Rect(x, y, width, height)
@@ -84,7 +70,6 @@ screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Game of Life")
 
 
-# Define colors (RGB format)
 white = (255, 255, 255)
 black = (0, 0, 0)
 red = (255, 0, 0)
@@ -93,7 +78,6 @@ blue = (0, 0, 255)
 gray = (127,127,127)
 orange = (255,100,10)
 
-# Create a pause button
 button_width, button_height = 100, 50
 pause_button = Button((width-button_width)//2, height-pannelHeight+(pannelHeight-button_height)//2, button_width, button_height, green, "Pause")
 resume_button = Button((width-button_width)//2, height-pannelHeight+(pannelHeight-button_height)//2, button_width, button_height, blue, "resume")
@@ -102,7 +86,6 @@ load_button = Button((3*width//2-button_width)//2, height-pannelHeight+(pannelHe
 
 
 array = np.zeros((heightSquares, widthSquares))
-# Main loop
 running = True
 paused = True
 clock = pygame.time.Clock()
@@ -152,7 +135,6 @@ while running:
                 array = gameOfLifeRules(array, counts, x, y)
                 color = orange if value == 1 else black
                 pygame.draw.rect(screen, color, ((x+1) * squareSize, (y+1) * squareSize, squareSize, squareSize))
-        #running = False
     pygame.display.flip()
     clock.tick(60)
     pygame.time.delay(100)
